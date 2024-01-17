4a9416824a7c4cf52f1dd28384546ae3ea8b30a0
GAZITASNIMAHMAD <tasnimahmad57@gmail.com>
2024-01-18 02:36:31 +0530

    added table

--- src/main/java/stockhelperservice/StockHelperController.java
+++ src/main/java/stockhelperservice/StockHelperController.java
@@ -2,14 +2,16 @@ package stockhelperservice;
 
 import com.opencsv.exceptions.CsvException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import stockhelperservice.entities.StockRatioAndFactor;
 import stockhelperservice.model.StockFileMap;
+import stockhelperservice.repositories.StockRatioRepository;
 import stockhelperservice.service.StockHelperService;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("stockhelper")
@@ -19,11 +21,22 @@ public class StockHelperController {
     @Autowired
     private StockHelperService stockHelperService;
 
+    @Autowired
+    private StockRatioRepository stockRatioRepository;
+
 
     @PostMapping("/upload")
     public List<StockFileMap> handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException, CsvException {
         List<StockFileMap> stockData= stockHelperService.processFile(file);
         return stockData;
 //        return ResponseEntity.ok("File uploaded and processed successfully!");
+    }
+
+    @GetMapping("/getRatio")
+    public Optional<StockRatioAndFactor> getRatio(@RequestParam("date") String date, @RequestParam("symbol") String symbol) throws IOException, CsvException {
+//        List<StockFileMap> stockData= stockHelperService.processFile(file);
+//        return stockData;
+//        return ResponseEntity.ok("File uploaded and processed successfully!");
+        return stockRatioRepository.findAllNtByQt(symbol, date);
     }
 }
--- src/main/java/stockhelperservice/service/StockHelperServiceImpl.java
+++ src/main/java/stockhelperservice/service/StockHelperServiceImpl.java
@@ -3,9 +3,12 @@ package stockhelperservice.service;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import stockhelperservice.entities.StockRatioAndFactor;
 import stockhelperservice.model.StockFileMap;
+import stockhelperservice.repositories.StockRatioRepository;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,8 +16,13 @@ import java.io.Reader;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 @Service
 public class StockHelperServiceImpl implements StockHelperService{
+
+    @Autowired
+    private StockRatioRepository stockRatioRepository;
+
     @Override
     public List<StockFileMap> processFile(MultipartFile file) throws IOException, CsvException {
         try (Reader reader = new InputStreamReader(file.getInputStream())) {
@@ -22,6 +30,21 @@ public class StockHelperServiceImpl implements StockHelperService{
             var csvData = csvReader.readAll().stream()
                     .map(data -> new StockFileMap(data[0], data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],data[14]))
                     .collect(Collectors.toList());
+
+            for(StockFileMap stock : csvData){
+                StockRatioAndFactor stockData = new StockRatioAndFactor();
+                stockData.setSymbol(stock.getSymbol());
+                stockData.setDate(stock.getDate());
+                int factor = (Double.parseDouble(stock.getClosePrice()) < Double.parseDouble(stock.getOpenPrice()) || Double.parseDouble(stock.getDeliveryPer()) < 30 ) ? -1 : 1;
+                stockData.setFactor(factor);
+                stockData.setAvg_close_price(stock.getClosePrice());
+                stockData.setSum_delivery_qnty(stock.getDeliveryQnty());
+                stockData.setAvg_open_price(stock.getOpenPrice());
+                double dqByNt = Double.parseDouble(stock.getDeliveryQnty())  / Double.parseDouble(stock.getNumberOfTrades());
+                stockData.setDq_by_nt(dqByNt);
+                stockData.setSum_of_trades(stock.getNumberOfTrades());
+                stockRatioRepository.save(stockData);
+            }
             return csvData;
         }
     }
